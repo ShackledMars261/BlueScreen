ce9e99658cb5e97a3e8d66544d70ac815fb9553b
lavavex <27239435+lavavex@users.noreply.github.com>
2025-02-10 20:34:53 -0600

    Update BlueScreenPS.ps1

--- BlueScreenPS.ps1
+++ BlueScreenPS.ps1
@@ -5,8 +5,7 @@ $logo = "
 /_____/   (  (
            \  \
             \__\"
-Write-Host $logo " Welcome to BlueScreen PWSH.
-Collecting Info, Please Wait..."
+
 
 $Passwords = "Zeus", "Athena", "Apollo", "Anubis", "Medusa", "Odin", "Hercules", "Aphrodite", "Poseidon", "Krishna",
              "Ra", "Shiva", "Hades", "Freyja", "Persephone", "Loki", "Artemis", "Osiris", "Horus", "Ganesh",
@@ -16,15 +15,164 @@ $Passwords = "Zeus", "Athena", "Apollo", "Anubis", "Medusa", "Odin", "Hercules",
 
 $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()_+"
 
-
-
-
 $hostname = hostname
 $user = whoami
-#find command for this that works please
-$ipaddr = "No Clue RN"
+$ipaddr = (Get-NetIPAddress | Where-Object { $_.AddressState -eq "Preferred" -and $_.AddressFamily -eq "IPv4" -and $_.IPAddress -NotContains "127.0.0.1"}).IPAddress
 
-Write-Host "Version:"$PSVersionTable.OS
-Write-Host "Hostname:"$hostname
-Write-Host "User:"$user
-Write-Host "IP Address:"$ipaddr
+function header {
+    Clear-Host
+    Write-Host $logo "`n`nWelcome to BlueScreen PWSH.`nCollecting Info, Please Wait..."
+    Write-Host "Version:" $PSVersionTable.OS
+    Write-Host "Hostname:" $hostname
+    Write-Host "User:" $user
+    Write-Host "IP Address:" $ipaddr
+}
+function prereqs {
+    Write-Host "Setting up Prerequisites"
+    Write-Host "Checking for Choco package manager"
+    if (Test-Path 'C:\ProgramData\chocolatey\choco.exe') {
+        Write-Host "Choco is already installed"
+        choco feature enable -n allowGlobalConfirmation
+        mainMenu
+    } else {
+        Write-Host "Choco is not installed"
+        installChoco
+    }
+}
+
+function installChoco {
+    Write-Host "Setting up Choco package manager"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    Write-Host "Choco Installed!"
+    Write-Host "Need to restart script for Choco to work"
+    Write-Host "Press any key to exit"
+    [void][System.Console]::ReadKey($true)
+}
+
+function mainMenu {
+    $mainMenu = 'X'
+    while($mainMenu -ne ''){
+        Clear-Host
+        header
+
+        Write-Host -ForegroundColor Cyan "Main Menu"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Programs"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Updates"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "3"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Lockdown"
+
+        $mainMenu = Read-Host "`nSelection (leave blank to quit)"
+        # Launch submenu1
+        if($mainMenu -eq 1){
+            subMenu1
+        }
+        # Launch submenu2
+        if($mainMenu -eq 2){
+            subMenu2
+        }
+        # Launch submenu3
+        if($mainMenu -eq 3){
+            subMenu3
+        }
+    }
+}
+
+function subMenu1 {
+    $subMenu1 = 'X'
+    while($subMenu1 -ne ''){
+        Clear-Host
+        header
+
+        Clear-Host
+        Write-Host -ForegroundColor Cyan "Programs"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Nmap"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Say goodbye"
+        $subMenu1 = Read-Host "`nSelection (leave blank to quit)"
+        $timeStamp = Get-Date -Uformat %m%d%y%H%M
+        # Option 1
+        if($subMenu1 -eq 1){
+            Write-Host 'Installing Nmap'
+            choco install nmap
+            # Pause and wait for input before going back to the menu
+            Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+        # Option 2
+        if($subMenu1 -eq 2){
+            Write-Host 'Goodbye!'
+            # Pause and wait for input before going back to the menu
+            Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+    }
+}
+
+function subMenu2 {
+    $subMenu2 = 'X'
+    while($subMenu2 -ne ''){
+        Clear-Host
+        header
+        Write-Host -ForegroundColor Cyan "Updates"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Show processes"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Show PS Version"
+        $subMenu2 = Read-Host "`nSelection (leave blank to quit)"
+        $timeStamp = Get-Date -Uformat %m%d%y%H%M
+        # Option 1
+        if($subMenu2 -eq 1){
+            Get-Process
+            # Pause and wait for input before going back to the menu
+            Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+        # Option 2
+        if($subMenu2 -eq 2){
+            $PSVersionTable.PSVersion
+            # Pause and wait for input before going back to the menu
+            Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+    }
+}
+
+function subMenu3 {
+    $subMenu2 = 'X'
+    while($subMenu2 -ne ''){
+        Clear-Host
+        header
+        Write-Host -ForegroundColor Cyan "Lockdown"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "1"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Show processes"
+        Write-Host -ForegroundColor DarkCyan -NoNewline "`n["; Write-Host -NoNewline "2"; Write-Host -ForegroundColor DarkCyan -NoNewline "]"; `
+            Write-Host -ForegroundColor DarkCyan " Show PS Version"
+        $subMenu2 = Read-Host "`nSelection (leave blank to quit)"
+        $timeStamp = Get-Date -Uformat %m%d%y%H%M
+        # Option 1
+        if($subMenu2 -eq 1){
+            Get-Process
+            # Pause and wait for input before going back to the menu
+            Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+        # Option 2
+        if($subMenu2 -eq 2){
+            $PSVersionTable.PSVersion
+            # Pause and wait for input before going back to the menu
+            Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
+            Write-Host "`nPress any key to return to the previous menu"
+            [void][System.Console]::ReadKey($true)
+        }
+    }
+}
+prereqs
+mainMenu
