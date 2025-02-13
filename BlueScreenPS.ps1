8dfaa0db2fa1c99c9509cf12a5ccd76c47302bd1
lavavex <27239435+lavavex@users.noreply.github.com>
2025-02-12 23:49:40 -0600

    Update BlueScreenPS.ps1

--- BlueScreenPS.ps1
+++ BlueScreenPS.ps1
@@ -232,7 +232,7 @@ function subMenu3 {
         }
         # Option 2
         if($subMenu3 -eq 2){
-            nmap -A $ipaddress/$subnet -vv -oN $timeStamp-nmap.txt
+            nmap -A $combineaddr -vv -oN $timeStamp-nmap.txt
             # Pause and wait for input before going back to the menu
             Write-Host -ForegroundColor DarkCyan "`nScript execution complete."
             Write-Host "`nPress any key to return to the previous menu"
