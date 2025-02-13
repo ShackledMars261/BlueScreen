beda7cb95889eaae4ae210a6c6cda3899b9ecb84
lavavex <27239435+lavavex@users.noreply.github.com>
2025-02-12 22:59:46 -0600

    Update BlueScreenPS.ps1

--- BlueScreenPS.ps1
+++ BlueScreenPS.ps1
@@ -27,7 +27,7 @@ $jsonURL = "https://raw.githubusercontent.com/MCA-Dev-Team/BlueScreen/refs/heads
 
 function updatescript {
     Invoke-WebRequest https://raw.githubusercontent.com/MCA-Dev-Team/BlueScreen/refs/heads/main/BlueScreenPS.ps1 -OutFile ~/Desktop/BlueScreenPS.ps1
-    Invoke-WebRequest https://raw.githubusercontent.com/MCA-Dev-Team/BlueScreen/refs/heads/main/script.json -OutFile ~/Desktop/script.json
+    Invoke-WebRequest $jsonURL -OutFile ~/Desktop/script.json
     Write-Host "Script Updated!"
     Write-Host "Press any key to exit"
     [void][System.Console]::ReadKey($true)
@@ -69,7 +69,7 @@ function ExplorerSetup {
 
 function RunWinUtil {
     Invoke-WebRequest -Uri $jsonURL -OutFile $jsonPath
-    invoke-expression 'cmd /c start powershell -NoProfile -ExecutionPolicy Bypass -Command {iwr christitus.com/win | iex -Config $jsonPath}'
+    powershell -NoProfile -ExecutionPolicy Bypass -Command {iwr christitus.com/win | iex -Config $jsonPath}
 }
 
 function mainMenu {
